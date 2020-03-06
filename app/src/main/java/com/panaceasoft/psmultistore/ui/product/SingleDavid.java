4f6b6e15281784dc7e849833aa840368c51191d8
David Almeyda <dav.almeyda@gmail.com>
2020-03-06 17:39:09 -0500

    entregado

---
+++ app/src/main/java/com/panaceasoft/psmultistore/ui/product/SingleDavid.java
@@ -0,0 +1,15 @@
+package com.panaceasoft.psmultistore.ui.product;
+
+public enum single {
+    INSTANCE;
+
+    int value;
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}

364c9b22e9b34f057d6869e2c47065f5f4252846
David Almeyda <dav.almeyda@gmail.com>
2020-03-06 10:37:20 -0500

    FINALIZANDO

---
+++ app/src/main/java/com/panaceasoft/psmultistore/ui/product/AgregarProductosActivity.java
@@ -0,0 +1,14 @@
+package com.panaceasoft.psmultistore.ui.product;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AgregarProductosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agregar_productos);
+    }
+}
